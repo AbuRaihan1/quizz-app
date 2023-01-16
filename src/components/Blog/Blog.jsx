b95c756807b13f40265847d2b857c9c8eb147f1d
farabi shiblu <shiblumd436@gmail.com>
2023-01-16 11:07:36 +0600

    edit readme file

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import './Blog.css'
+import React from "react";
+import "./Blog.css";
 const Blog = () => {
-    return (
-        <div>
-          <h2>blog</h2>
+  return (
+    <div>
+      <div className="blogjWrapper">
+        <div className="commonQue">
+          <h3>1. What is the purpose of react router?</h3>
+          <p>
+            The Router in React JS is primarily used to create Single Page Web
+            Apps. In the application, React Router is utilized to define various
+            routes. When a user enters a URL into your browser and the URL route
+            equals one of several 'pathways' as in the router folder, the user
+            is sent to that route.
+          </p>
         </div>
-    );
+
+        <div className="commonQue">
+          <h3>1. What is the purpose of react router?</h3>
+          <p>
+            The Router in React JS is primarily used to create Single Page Web
+            Apps. In the application, React Router is utilized to define various
+            routes. When a user enters a URL into your browser and the URL route
+            equals one of several 'pathways' as in the router folder, the user
+            is sent to that route.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
