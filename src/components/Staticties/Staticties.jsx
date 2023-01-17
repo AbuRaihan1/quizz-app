41e891ff32918041f67603c04de1f4881dbe9b9a
Abu Raihan <raihanahmed01973@gmail.com>
2023-01-17 19:37:20 +0600

    loads and show rechart in ui

--- src/components/Staticties/Staticties.jsx
+++ src/components/Staticties/Staticties.jsx
@@ -1,11 +1,84 @@
-import React from 'react';
-import './Staticties.css'
+import React, { useContext } from "react";
+import { TopicsContext } from "../../layout/Main";
+import "./Staticties.css";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Legend,
+} from "recharts";
+
 const Staticties = () => {
-    return (
-        <div>
-            <h2>staticties</h2>
-        </div>
-    );
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+  const loadTopicsData = useContext(TopicsContext);
+  
+  console.log(loadTopicsData);
+  return (
+    <div>
+      <h2>staticties</h2>
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Legend />
+
+        <Bar dataKey="uv" fill="#ff5200" />
+      </BarChart>
+    </div>
+  );
 };
 
 export default Staticties;
