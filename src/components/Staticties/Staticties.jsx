28103c63a55b7f275e3517ddd22d7da8288bfecc
Abu Raihan <raihanahmed01973@gmail.com>
2023-01-17 21:41:00 +0600

    done staticties page with responsive

--- src/components/Staticties/Staticties.jsx
+++ src/components/Staticties/Staticties.jsx
@@ -1,7 +1,14 @@
-import React, { useContext, } from "react";
+import React, { useContext } from "react";
 import { TopicsContext } from "../../layout/Main";
 import "./Staticties.css";
-import { BarChart, Bar, XAxis, YAxis, Legend } from "recharts";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const Staticties = () => {
   const loadTopicsData = useContext(TopicsContext);
@@ -12,29 +19,17 @@ const Staticties = () => {
     };
     return singleTopic;
   });
-  console.log(topicBarData);
-
   return (
-    <div>
+    <div className="question">
       <h2>staticties</h2>
-      <BarChart
-        width={500}
-        height={300}
-        data={topicBarData}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <XAxis dataKey="name" />
-        <YAxis dataKey="question" />
-        <Legend />
-
-        {/* <Bar dataKey="name" fill="#ff5200" /> */}
-        <Bar dataKey="question" fill="#ff5200" />
-      </BarChart>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart data={topicBarData}>
+          <XAxis dataKey="name" />
+          <YAxis dataKey="question" />
+          <Legend />
+          <Bar dataKey="question" fill="#ff5200" />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -2,12 +2,12 @@ import React, { useContext } from "react";
 import { TopicsContext } from "../../layout/Main";
 import Topic from "./Topic";
 import "./Topics.css";
-import coverImg from "../../../src/quiz.jpg";
+// import coverImg from "../../../src/quiz.jpg";
 const Topics = () => {
   const topicsData = useContext(TopicsContext);
   return (
     <>
-      <img src={coverImg} alt="" width={"70%"} height={'400'}/>
+      {/* <img src={coverImg} alt="" width={"70%"} height={'400'}/> */}
       <div className="mainWrapTotal">
         {topicsData.map((topic) => (
           <Topic topic={topic} key={topic.id}></Topic>
